5cb3d2e559836e42c475699164a0f5a886a03bc1
idwesar <79699158+idwesar@users.noreply.github.com>
2021-03-16 13:16:01 +0000

    Update adventure - entrance working.py

--- adventure - entrance working.py
+++ adventure - entrance working.py
@@ -10,55 +10,41 @@ def start():
 
 def  entrance():
     global fire
-    global hp
-    global causeofdeath
     if fire:
         entrance = input("What do you do?: [1] Enter the cave, [2] Go out into the blizzard, [3] Sit by the fire for a while.")
-        if entrance == "1":
-            print("\nYou enter the cave. The sound of howling wind fades away.")
-            cave1()
-        elif entrance == "2":
-            print("\nYou walk out into the blizzard, shielding your face from the stinging snow. You feel the cold seep into your bones. HP - 10. ")
-            hp -= 10
-            print(f"HP: {hp}")
-            if hp == 0:
-                causeofdeath = "Froze to death"
-                death()
-            else:
-                blizzard()
-        elif entrance == "3":
-            if hp < 100:
-                hp += 10
-                print("\nYou sit by the fire for a while. The warmth fills your bones and you feel your strength returning.")
-                print(f"Health: {hp}")
-            else:
-                print("\nYou sit by the fire for a while, contemplating your next move.")
-            entrance()
-        else:
-            invalid()
-            entrance()
     else:
         entrance =  input("What do you do?: [1] Enter the cave, [2] Go out into the blizzard, [3] Start a fire, [4] Wait here a while.")
-        if entrance == "1":
-            print("\nYou enter the cave. The sound of howling wind fades away as you head into the darkness. The light of the entrance fades too...if only you could bring some with you...")
-            cave1()
-        elif entrance == "2":
-            blizzard()
-        elif entrance == "3":
-            fire = True
-            if hp < 100:
-                print("\nYou decide to light a fire. The warmth fills your bones and you feel your strength returning.")
-                hp += 10
-                print(f"Health: {hp}")
-            else:
-                print("\nYou decide to light a fire. The warmth fills your bones.")
-            entrance()
-        elif entrance == "4":
-            print("\nYou hang out in the shelter of the cave entrance for a while.")
-            entrance()
+    if entrance == "1":
+        if fire:
+            print("\nYou enter the cave. The sound of howling wind fades away.")
         else:
-            invalid()
-            entrance()
+            print("\nYou enter the cave. The sound of howling wind fades away as you head into the darkness. The light of the entrance fades too...if only you could bring some with you...")
+        cave1()
+    elif entrance == "2":
+        blizzard()
+    elif entrance == "3":
+        if fire:
+            print("\nYou decide to sit by the fire for a while.")
+        else:
+            fire = True
+            print("\nYou decide to light a fire.")
+        sitbyfire()
+    elif entrance == "4":
+        print("\nYou hang out in the shelter of the cave entrance for a while.")
+        entrance()
+    else:
+        invalid()
+        entrance()
+
+def sitbyfire():
+    global hp
+    if hp < 100:
+        hp += 10
+        print("The warmth fills your bones and you feel your strength returning.")
+        print(f"Health: {hp}")
+    else:
+        print("\nThe warmth washes over you as you contemplate your next move.")
+    entrance()
 
 def death():
     global causeofdeath
@@ -70,23 +56,33 @@ def replay():
     if replay == "1":
         start()
     elif replay == "2":
-        print("Thanks for playing!")
+        print("\nThanks for playing!")
     else:
         invalid()
         replay()
     
 def blizzard():
-    blizzard = input("What do you do?: [1] Run back to the cave, [2] Walk further into the blizzard.")
-    if blizzard == "1":
-        print("\nYou run back to the cave as fast as you can - that was a horrible idea. You collapse on the floor of the cave, shivering.")
-        entrance()
-    elif blizzard == "2":
+    global hp
+    print("\nYou walk into the blizzard, shielding your face from the stinging snow. You feel the cold seep into your bones. HP - 10. ")
+    hp -= 10
+    print(f"HP: {hp}")
+    if hp == 0:
         global causeofdeath
         causeofdeath = "Froze to death"
         death()
     else:
-        invalid()
+        blizzard_action()
+
+def blizzard_action():
+    action = input("What do you do?: [1] Run back to the cave, [2] Walk further into the blizzard.")
+    if action == "1":
+        print("\nYou run back to the cave as fast as you can - that was a horrible idea. You collapse on the floor of the cave, shivering.")
+        entrance()
+    elif action == "2":
         blizzard()
+    else:
+        invalid()
+        blizzard_action()
 
 def invalid():
     print("\nNot a valid input. Try again.")
